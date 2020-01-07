6768cfabf624053e749873ac1ac843e2ab670374
mnervik <nervik.mathias@gmail.com>
2020-01-07 19:35:34 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Simple Title</h1>
+            </div>
+        )
+    }
+}
+
+export default App
