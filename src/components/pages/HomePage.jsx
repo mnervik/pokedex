2cb051b5d163c95461dcbdd01b642e68afdef83c
mnervik <nervik.mathias@gmail.com>
2020-01-08 16:28:01 +0100

    Working results-page from API
    Implemented best practice for objects
    Set limit in state, instead of hardcoded
    Fixed: `sprite` was referenced as `img`...changed to `img`
    Temporarily removed debug events

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -10,14 +10,11 @@ class HomePage extends Component {
             weight: 0,
             img: "",
         }],
+        limit: 10
     }
 
     componentDidMount() {
         this.getData()
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        //console.log(this.state)
     }
 
     render() {
@@ -25,27 +22,40 @@ class HomePage extends Component {
             <div>
                 <h1>HomePage</h1>
 
-                {this.printData()}
+                <div className="row">
+                    {this.state.pokemon.map(pokemon => (
+                        <div className="col-2">
+                            <div className="card">
+                                <img className="card-img-top" src={pokemon.img} alt="pokemon"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{pokemon.name}</h5>
+                                    <p className="card-text">weight: {pokemon.weight}</p>
+                                    <Link className="btn btn-primary" to={`/pokemon/${pokemon.id}`}>
+                                        More Info
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
 
     getData() {
-        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=2')
+        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${this.state.limit}`)
             .then(({data: {results: data}}) => {
                 this.clearData()
 
                 for (let i = 0; i < data.length; i++) {
                     axios.get(data[i].url)
                         .then(({data}) => {
-                            //console.log(data)
-
                             this.setState({
                                 pokemon: [...this.state.pokemon, {
                                     id: data['id'],
                                     name: data['name'],
                                     weight: data['weight'],
-                                    sprite: data['sprites']['front_default'],
+                                    img: data['sprites']['front_default'],
                                 }]
                             })
                         })
@@ -58,25 +68,6 @@ class HomePage extends Component {
         this.setState({
             pokemon: []
         })
-    }
-
-    printData() {
-        const arr = [];
-
-        /*<div className="row">
-            <div className="col-2">
-                <div className="card">
-                    <img className="card-img-top" src={this.state.pokemon[0].img}/>
-                    <div className="card-body">
-                        <h5 className="card-title">{this.state.pokemon[0].name}</h5>
-                        <p className="card-text">weight: {this.state.pokemon[0].weight}</p>
-                        <Link className="btn btn-primary" to={`/pokemon/${this.state.pokemon[0].id}`}>
-                            More Info
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>*/
     }
 }
 
