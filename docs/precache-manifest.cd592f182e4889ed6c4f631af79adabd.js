acf4d4dbf8ef516ccd120535b819d92930cbb197
mnervik <nervik.mathias@gmail.com>
2020-01-13 15:53:24 +0100

    Added Demo Page

---
+++ docs/precache-manifest.cd592f182e4889ed6c4f631af79adabd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238e94eb631c4ec9ec6c78a3a43339ca",
+    "url": "/pokedex/docs/index.html"
+  },
+  {
+    "revision": "7a60d7dfd449c3736dd3",
+    "url": "/pokedex/docs/static/css/main.0a9844ec.chunk.css"
+  },
+  {
+    "revision": "daccb59ed3c8cec51699",
+    "url": "/pokedex/docs/static/js/2.a7433186.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pokedex/docs/static/js/2.a7433186.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a60d7dfd449c3736dd3",
+    "url": "/pokedex/docs/static/js/main.4638740b.chunk.js"
+  },
+  {
+    "revision": "87445f107b8fb77a10ea",
+    "url": "/pokedex/docs/static/js/runtime-main.59ea6037.js"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/pokedex/docs/static/media/Pokemon Hollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokedex/docs/static/media/Pokemon Solid.36d26dbc.ttf"
+  }
+]);
